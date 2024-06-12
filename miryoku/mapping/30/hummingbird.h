7a8b2689c21dcd860a54ef27133de8bfd8ad0abd
Sergey <cherv277@gmail.com>
2024-06-12 21:01:54 +0400

    Add weird hummingbird remap for Tern keyboards

--- miryoku/mapping/30/hummingbird.h
+++ miryoku/mapping/30/hummingbird.h
@@ -1,6 +1,10 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// WARNING!!! THIS WEIRD MAPPING IS DESIGNED SPECIFICALLY
+// FOR USAGE WITH TERN KEYBOARD (https://github.com/rschenk/tern/tree/main)
+// DON'T USE IT WITH OTHER BOARDS!!!
+
 #if !defined (MIRYOKU_LAYOUTMAPPING_HUMMINGBIRD)
 
 #define MIRYOKU_LAYOUTMAPPING_HUMMINGBIRD( \
@@ -9,8 +13,8 @@ K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
 N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
-K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K10  K01  K02  K03  K04       K05  K06  K07  K08  K19 \
+K00  K11  K12  K13  K14       K15  K16  K17  K18  K09 \
      K21  K22  K23                 K26  K27  K28 \
                K33  K34       K35  K36
 
